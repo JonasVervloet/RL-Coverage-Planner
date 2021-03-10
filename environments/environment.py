2ad39d0fa37bfb27dafde9509859b2d5a3451342
JonasVervloet <vervloetjonas@hotmail.com>
2021-03-10 12:03:42 +0100

    environment

---
+++ environments/environment.py
@@ -0,0 +1,135 @@
+import random
+import numpy as np
+
+from environments.env_representation import EnvironmentRepresentation
+
+class Environment:
+
+    MOVE_PUNISHMENT = 0.05
+    OBSTACLE_PUNISHMENT = 0.5
+
+    DISCOVER_REWARD = 1.0
+    COVERAGE_REWARD = 50.0
+
+    MAX_STEP_MULTIPLIER = 2
+
+    def __init__(self, generator=None):
+        self.generator = generator
+
+        self.env_info = None
+        self.current_pos = None
+        self.nb_steps = 0
+        self.done = True
+
+        self.single_env = False
+
+    def set_environment_representation(self, representation):
+        self.env_info = representation
+        self.single_env = True
+
+    def reset(self):
+        self.done = False
+        if not self.single_env:
+            self.env_info = self.generator.generate_environment()
+        self.current_pos = random.sample(self.env_info.start_positions, 1)[0]
+        self.visited_tiles = np.zeros_like(self.env_info.obstacle_map)
+        self.visited_tiles[self.current_pos] = 1
+        self.nb_steps = 0
+
+        return self.get_state()
+
+    def get_state(self):
+        if self.env_info.terrain_map is not None:
+            raise Exception("NOT SUPPORTED YET")
+        else:
+            current_pos_grid = np.zeros_like(self.env_info.obstacle_map)
+            current_pos_grid[self.current_pos] = 1
+
+            return np.stack([current_pos_grid, self.visited_tiles, self.env_info.obstacle_map])
+
+    def has_complete_coverage(self, n_pos):
+        check = np.ones_like(self.env_info.obstacle_map)
+        added_map = self.visited_tiles + self.env_info.obstacle_map
+        added_map[n_pos] = 1
+
+        if np.sum(added_map - check) == 0:
+            return True
+
+    def get_reward(self, n_pos):
+        reward = -self.MOVE_PUNISHMENT
+        if self.env_info.obstacle_map[n_pos] == 1:
+            reward -= self.OBSTACLE_PUNISHMENT
+        else:
+            if self.env_info.obstacle_map[n_pos] == 0 and self.visited_tiles[n_pos] == 0:
+                reward += self.DISCOVER_REWARD
+
+                if self.has_complete_coverage(n_pos):
+                    reward += self.COVERAGE_REWARD
+
+        return reward
+
+    def is_done(self, n_pos):
+        if self.env_info.obstacle_map[n_pos] == 1:
+            self.done = True
+            return True
+
+        if self.has_complete_coverage(n_pos):
+            self.done = True
+            return True
+
+        if self.nb_steps > self.MAX_STEPS_MULTIPLIER * self.env_info.nb_free_tiles:
+            self.done = True
+            return True
+
+        return False
+
+    def step(self, action):
+        if self.done:
+            raise Exception("ENVIRONMENT: should reset first...")
+
+        if action == 0:
+            n_position = (max(0, self.current_pos[0] - 1), self.current_pos[1])
+        elif action == 1:
+            n_position = (min(self.env_info.obstacle_map.shape[0] - 1, self.current_pos[0] + 1), self.current_pos[1])
+        elif action == 2:
+            n_position = (self.current_pos[0], max(0, self.current_pos[1] - 1))
+        elif action == 3:
+            n_position = (self.current_pos[0], min(self.env_info.obstacle_map.shape[1] - 1, self.current_pos[1] + 1))
+        else:
+            raise Exception("ACTION NOT SUPPORTED!")
+
+        self.nb_steps += 1
+
+        reward = self.get_reward(n_position)
+        done = self.is_done(n_position)
+
+        if self.env_info.obstacle_map[n_position] == 0 and self.visited_tiles[n_position] == 0:
+            self.visited_tiles[n_position] = 1
+
+        self.current_pos = n_position
+
+        return self.get_state(), reward, done
+
+
+if __name__ == "__main__":
+    save_path = "D:/Documenten/Studie/2020-2021/Masterproef/Reinforcement-Learner-For-Coverage-Path-Planning/data/"
+
+    env_representation = EnvironmentRepresentation()
+    env_representation.load(save_path, "test_representation")
+
+    env = Environment()
+    env.set_environment_representation(env_representation)
+
+    state = env.reset()
+    print(state.shape)
+    print(env.current_pos)
+    print(np.sum(env.visited_tiles))
+    print(env.nb_steps)
+
+    state, reward, done = env.step(0)
+    print(state.shape)
+    print(reward)
+    print(done)
+    print(env.current_pos)
+    print(env.nb_steps)
+    print(np.sum(env.visited_tiles))
--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import sys, getopt
 import matplotlib.pyplot as plt
 
 from environments.env_generation import EnvironmentGenerator
+from environments.env_representation import EnvironmentRepresentation
+from environments.environment import Environment
 
 SHORT_OPTIONS = ""
 LONG_OPTIONS = [
@@ -9,7 +11,13 @@ LONG_OPTIONS = [
     "dim=",
     "hFreq=",
     "oFreq=",
-    "fillRatio="
+    "fillRatio=",
+    "loadEnv=",
+    "movePunish=",
+    "obstaclePunish=",
+    "discoverReward=",
+    "coverageReward=",
+    "maxStepMultiplier="
 ]
 
 def main(argv):
@@ -24,6 +32,13 @@ def main(argv):
     obstacle_frequency = (2, 2)
     fill_ratio = 0.14
 
+    env_repr = None
+
+    move_punishment = 0.05
+    obstacle_punishment = 0.5
+    discover_reward = 1.0
+    coverage_reward = 50.0
+    max_step_multiplier = 2
 
     for option, argument in options:
         if option == "--heightRequired":
@@ -44,11 +59,41 @@ def main(argv):
         if option == "--fillRatio":
             fill_ratio = float(argument)
 
+        if option == "--loadEnv":
+            arg_split = argument.split(",")
+            env_repr = EnvironmentRepresentation()
+            env_repr.load(arg_split[0], arg_split[1])
+
+        if option == "--movePunish":
+            move_punishment = float(argument)
+
+        if option == "--obstaclePunish":
+            obstacle_punishment = float(argument)
+
+        if option == "--discoverReward":
+            discover_reward = float(argument)
+
+        if option == "--coverageReward":
+            coverage_reward = float(argument)
+
+        if option == "--maxStepMultiplier":
+            max_step_multiplier = int(argument)
+
     env_generator = EnvironmentGenerator(height_required)
     env_generator.set_dimension(dimension)
     env_generator.set_height_frequency(height_frequency)
     env_generator.set_obstacle_frequency(obstacle_frequency)
     env_generator.set_fill_ration(fill_ratio)
+
+    env = Environment(env_generator)
+    if env_repr is not None:
+        env.set_environment_representation(env_repr)
+
+    env.MOVE_PUNISHMENT = move_punishment
+    env.OBSTACLE_PUNISHMENT = obstacle_punishment
+    env.DISCOVER_REWARD = discover_reward
+    env.COVERAGE_REWARD = coverage_reward
+    env.MAX_STEP_MULTIPLIER = max_step_multiplier
 
     fig, axs = plt.subplots(2, 2)
 
