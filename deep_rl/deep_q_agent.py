38a8e5c2843a41bee10e3681a5a87b5a5e4a3db7
JonasVervloet <vervloetjonas@hotmail.com>
2021-03-14 17:49:15 +0100

    DQN + trainer

---
+++ deep_rl/deep_q_agent.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn.functional as F
+from collections import deque
+import random
+import math
+
+from deep_rl.trainer import Transition as Transition
+
+
+class DeepQAgent:
+
+    QUEUE_LENGTH = 5000
+    GAMMA = 1.0
+    EPSILON_START = 0.9
+    EPSILON_END = 0.05
+    EPSILON_DECAY = 2000
+    TARGET_UPDATE = 1000
+
+    BATCH_SIZE = 32
+
+    def __init__(self, network_generator, optim_class, nb_actions):
+        self.policy_net = network_generator.generate_network()
+        self.target_net = network_generator.generate_network()
+
+        self.nb_actions = nb_actions
+
+        self.replay_buffer = deque(maxlen=DeepQAgent.QUEUE_LENGTH)
+        self.epsilon = DeepQAgent.EPSILON_START
+        self.optimizer = optim_class(self.policy_net.parameters())
+        self.batch_size = DeepQAgent.BATCH_SIZE
+
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+        self.step_counter = 0
+
+    def select_action(self, state_tensor):
+        if random.random() > self.epsilon:
+            with torch.no_grad():
+                state_eval = self.policy_net(torch.unsqueeze(state_tensor, 0))[0]
+                return torch.argmax(state_eval).item()
+
+        return random.randrange(self.nb_actions)
+
+    def update_epsilon(self, episode_nb):
+        diff = DeepQAgent.EPSILON_START - DeepQAgent.EPSILON_END
+        self.epsilon = DeepQAgent.EPSILON_END + diff * math.exp(-1 * episode_nb / DeepQAgent.EPSILON_DECAY)
+
+    def save(self, path, episode_nb):
+        torch.save(self.policy_net.state_dict(), path + f"deep_q_agent_{episode_nb}.pt")
+
+    def load(self, path, episode_nb):
+        self.policy_net.load_state_dict(torch.load(path + f"deep_q_agent_{episode_nb}.pt"))
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+    def evaluate(self):
+        self.epsilon = 0.0
+
+    def observe_transition(self, transition):
+        self.replay_buffer.append(transition)
+
+        if len(self.replay_buffer) <= self.batch_size * 10:
+            return
+
+        transitions = random.sample(self.replay_buffer, self.batch_size)
+        mini_batch = Transition(*zip(*transitions))
+
+        state_batch = torch.stack(mini_batch.state)
+        action_batch = torch.stack(mini_batch.action)
+        action_batch = action_batch.unsqueeze(1)
+        reward_batch = torch.stack(mini_batch.reward)
+        next_state_batch = torch.stack(mini_batch.next_state)
+        non_final_mask = ~torch.stack(mini_batch.done)
+
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        next_state_values = torch.zeros(self.batch_size)
+        next_state_values[non_final_mask] = torch.max(self.target_net(next_state_batch), dim=1)[0][non_final_mask]
+        expected_values = (next_state_values * DeepQAgent.GAMMA) + reward_batch
+
+        loss = F.smooth_l1_loss(state_action_values, expected_values.unsqueeze(1))
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if self.step_counter % DeepQAgent.TARGET_UPDATE == 0:
+            self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.step_counter += 1
---
+++ deep_rl/trainer.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from collections import namedtuple
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'done'))
+
+
+class DeepRLTrainer:
+
+    NB_EPISODES = 3000
+    SAVE_EVERY = 500
+    INFO_EVERY = 50
+
+    def __init__(self, environment, agent, save_path):
+        self.env = environment
+        self.agent = agent
+
+        self.total_rewards = []
+        self.avg_rewards = []
+
+        self.tiles_visited = []
+        self.avg_tiles_visited = []
+
+        self.nb_steps = []
+        self.avg_nb_steps = []
+
+        self.cc_counter = 0
+        self.nb_complete_cov = []
+
+        self.save_path = save_path
+
+    def train(self):
+        for episode_nb in range(DeepRLTrainer.NB_EPISODES + 1):
+
+            current_state = torch.tensor(self.env.reset(), dtype=torch.float)
+            done = False
+            total_reward = 0
+            nb_steps = 0
+
+            self.agent.update_epsilon(episode_nb)
+
+            while not done:
+                action = self.agent.select_action(current_state)
+                n_state, reward, done = self.env.step(action)
+
+                total_reward += reward
+                nb_steps += 1
+
+                action = torch.tensor(action, dtype=torch.int64)
+                n_state = torch.tensor(n_state, dtype=torch.float)
+                reward = torch.tensor(reward, dtype=torch.float)
+                done = torch.tensor(done, dtype=torch.bool)
+
+                self.agent.observe_transition(Transition(
+                    current_state, action, n_state, reward, done
+                ))
+
+                current_state = n_state
+
+            if self.env.complete_coverage():
+                self.cc_counter += 1
+                print(f"COMPLETE COVERAGE: {self.cc_counter}")
+
+            self.total_rewards.append(total_reward)
+            self.nb_steps.append(nb_steps)
+            self.tiles_visited.append(self.env.get_nb_visited_tiles())
+            self.nb_complete_cov.append(self.cc_counter)
+
+            if episode_nb < DeepRLTrainer.SAVE_EVERY:
+                self.avg_rewards.append(np.average(self.total_rewards))
+                self.avg_tiles_visited.append(np.average(self.tiles_visited))
+                self.avg_nb_steps.append(np.average(self.nb_steps))
+            else:
+                self.avg_rewards.append(np.average(self.total_rewards[-DeepRLTrainer.SAVE_EVERY:]))
+                self.avg_tiles_visited.append(np.average(self.tiles_visited[-DeepRLTrainer.SAVE_EVERY:]))
+                self.avg_nb_steps.append(np.average(self.nb_steps[-DeepRLTrainer.SAVE_EVERY:]))
+
+            if episode_nb % DeepRLTrainer.INFO_EVERY == 0:
+                print(f"Episode {episode_nb}")
+                print(f"total reward: {total_reward}")
+                print(f"nb steps: {nb_steps}")
+                print(f"tiles visited: {self.tiles_visited[episode_nb]}")
+                print(f"epsilon: {self.agent.epsilon}")
+                print()
+
+            if episode_nb % DeepRLTrainer.SAVE_EVERY == 0:
+                x = range(episode_nb + 1)
+
+                plt.clf()
+                plt.plot(x, self.total_rewards, x, self.avg_rewards)
+                plt.legend(['total rewards', 'average total rewards'])
+                plt.title('Total reward for every episode')
+                plt.savefig(self.save_path + f"reward_episode{episode_nb}.png")
+                np.save(self.save_path + f"rewards_episode{episode_nb}.npy", self.total_rewards)
+                np.save(self.save_path + f"avg_rewards_episode{episode_nb}.npy", self.avg_rewards)
+
+                plt.clf()
+                plt.plot(x, self.tiles_visited, x, self.avg_tiles_visited)
+                plt.legend(['nb tiles visited', 'average nb tile visited'])
+                plt.title('Number of tiles visited for every episode')
+                plt.savefig(self.save_path + f"tiles_visited_episode{episode_nb}.png")
+                np.save(self.save_path + f"tiles_visited_episode{episode_nb}.npy", self.tiles_visited)
+                np.save(self.save_path + f"avg_tiles_visited_episode{episode_nb}.npy", self.avg_tiles_visited)
+
+                plt.clf()
+                plt.plot(x, self.nb_steps, x, self.avg_nb_steps)
+                plt.legend(['nb steps', 'average nb steps'])
+                plt.title('Number of steps for every episode')
+                plt.savefig(self.save_path + f"nb_steps_episode{episode_nb}.png")
+                np.save(self.save_path + f"nb_steps_episode{episode_nb}.npy", self.nb_steps)
+                np.save(self.save_path + f"avg_nb_steps_episode{episode_nb}.npy", self.avg_nb_steps)
+
+                plt.clf()
+                plt.plot(x, self.nb_complete_cov)
+                plt.legend(['nb complete coverage runs'])
+                plt.title('Nb of complete coverage runs')
+                plt.savefig(self.save_path + f"nb_complete_cov_episode{episode_nb}.png")
+                np.save(self.save_path + f"nb_complete_cov_episode{episode_nb}.npy", self.nb_complete_cov)
+
+                self.agent.save(self.save_path, episode_nb)
+
+
